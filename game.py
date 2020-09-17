d123bf39126fb9141e968ef112258cd82cc4a043
Kyle Tenzeldam <ktenzel@gmail.com>
2020-09-16 21:56:36 -0700

    updated logic

--- game.py
+++ game.py
@@ -1,10 +1,36 @@
 from sys import exit
-sword = True
 
 def drop_sword():
-    sword = False
     print("You've dropped your sword")
-    field()
+    dead("Without a sword you will surly lose.")
+
+def desert():
+    print("You head to a desert.")
+    print("The sun scorches you skin.")
+    print("You see an oacis.")
+    print("What do you do?")
+    print("Take a drink or head for th hills")
+    choice = input("> ")
+
+    if choice == "take a drink" or "1" in choice:
+        drink()
+    elif choice == "hills" or "2" in choice:
+        hills()
+    else:
+        drop_sword()
+
+
+def drink():
+    print("The waters renew your energy")
+    print("You leave the Oasis")
+    print("Head to the hills or ocean.")
+
+    if choice == "hills" in choice:
+        hills()
+    elif choice == "ocean" in choice:
+        ocean()
+    else:
+        drop_sword()
 
 def start():
     print("It's dangerous to go alone.")
@@ -15,13 +41,17 @@ def start():
 
     choice = input("> ")
 
-    if choice == "desert" or "1":
+    if choice == "desert" in choice:
         desert()
-    elif choice == "hills" or "2":
+    elif choice == "hills" in choice:
         hills()
-    elif choice == "ocean" or "3":
+    elif choice == "ocean" in choice:
         ocean()
     else:
         drop_sword()
+
+def dead(why):
+    print(why, "and so paseth you.")
+    exit(0)
 
 start()
