7443335a5fedae66b5c8941e80ac7c1e33e14f75
Kyle Tenzeldam <ktenzel@gmail.com>
2020-09-16 23:04:17 -0700

    testing start function

--- game.py
+++ game.py
@@ -1,57 +1,69 @@
 from sys import exit
 
-def drop_sword():
-    print("You've dropped your sword")
-    dead("Without a sword you will surly lose.")
-
-def desert():
-    print("You head to a desert.")
-    print("The sun scorches you skin.")
-    print("You see an oacis.")
-    print("What do you do?")
-    print("Take a drink or head for th hills")
-    choice = input("> ")
-
-    if choice == "take a drink" or "1" in choice:
-        drink()
-    elif choice == "hills" or "2" in choice:
-        hills()
-    else:
-        drop_sword()
-
-
-def drink():
-    print("The waters renew your energy")
-    print("You leave the Oasis")
-    print("Head to the hills or ocean.")
-
-    if choice == "hills" in choice:
-        hills()
-    elif choice == "ocean" in choice:
-        ocean()
-    else:
-        drop_sword()
-
-def start():
-    print("It's dangerous to go alone.")
-    print("Take this...")
-    print("aquired sword.")
-    print("What to do now?")
-    print("Head to the desert, hills or ocean?")
+def gold_room():
+    print("This room is full of gold.  How much do you take?")
 
     choice = input("> ")
-
-    if choice == "desert" in choice:
-        desert()
-    elif choice == "hills" in choice:
-        hills()
-    elif choice == "ocean" in choice:
-        ocean()
+    if "0" in choice or "1" in choice:
+        how_much = int(choice)
     else:
-        drop_sword()
+        dead("Man, learn to type a number.")
+
+    if how_much < 50:
+        print("Nice, you're not greedy, you win!")
+        exit(0)
+    else:
+        dead("You greedy bastard!")
+
+def bear_room():
+    print("There is a bear here.\nThe bear has a bundh of honey.\nThe fat bear is in fornt of another door. \nHow are you going to move the bear?")
+    bear_moved = False
+
+    while True:
+        choice = input("> ")
+
+        if choice == "take honey":
+            dead("The bear looks at you then slaps your face off")
+        elif choice == "taunt bear" and not bear_moved:
+            print("Bear has moved from the door.\nYou can go through it now.")
+            bear_moved = True
+        elif choice == "taunt bear" and bear_moved:
+            dead("The bear gets pissed off and chews your leg off.")
+        elif choice == "open door" and bear_moved:
+            gold_room()
+        else:
+            print("I got no idea what that means.")
+
+def cthulhu_room():
+    print("Here you see the great evil Cthulhu.")
+    print("He, it, whatever startes at you and you go insane")
+    print("Do you flee for your life or eat your head?")
+
+    choice = input("> ")
+
+    if "flee" in choice:
+        start()
+    elif "head" in choice:
+        dead("Well that was tasty")
+    else:
+        cthulhu_room()
 
 def dead(why):
-    print(why, "and so paseth you.")
+    print(why, "Good job!")
     exit(0)
+
+def start():
+    print("You are in a dark room.")
+    print("There is a door to your right and left.")
+    print("Which one do you take?")
+
+    choice = input(">")
+
+    if choice == "left":
+        bear_room()
+    elif choice == "right":
+        cthulhu_room()
+    else:
+        deadt("You stumble around the room until you starve.")
 
 start()
